2c6ba6d80be1e288e63b0b89d9bcbaba8ffa0675
Chris Leveille <cdleveille@gmail.com>
2025-06-14 05:36:14 -0400

    exclude tanstack start folders

--- .output/server/chunks/_/_tanstack-start-manifest_v-DVZNNO1M.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "__root.tsx", "children": ["/"], "preloads": ["/assets/main-CM1I1cG0.js"], "assets": [{ "tag": "script", "attrs": { "src": "/assets/main-CM1I1cG0.js", "type": "module" } }] }, "/": { "filePath": "index.tsx" } } });
-
-export { tsrStartManifest };
-//# sourceMappingURL=_tanstack-start-manifest_v-DVZNNO1M.mjs.map
